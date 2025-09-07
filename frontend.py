fdaa618914cfbf82ed9af635b0e71fabaf44debc
Aryan <aryanbhardwaj3166@gmail.com>
2025-09-07 14:43:03 +0530

    Local to repo

--- frontend.py
+++ frontend.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import requests
 
+BACKEND_URL = "http://localhost:8000/ask"
+
 st.set_page_config(page_title="Multimodal AI Therapist", page_icon=":robot_face:")
 st.title("Multimodal AI Therapist")
 
@@ -11,8 +13,8 @@ user_input = st.chat_input("What would you like to talk about?")
 if user_input:
     st.session_state.chat_history.append({"role": "user", "content": user_input})
 
-    fixed_answer = "This is a fixed response from the AI therapist."
-    st.session_state.chat_history.append({"role": "assistant", "content": fixed_answer})
+    response = requests.post(BACKEND_URL, json={"message": user_input})
+    st.session_state.chat_history.append({"role": "assistant", "content": f'{response.json()["response"]} WITH TOOL: [{response.json()["tool_used"]}]'})
 
 for msg in st.session_state.chat_history:
     with st.chat_message(msg["role"]):
--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-def main():
-    print("Hello from multimodal-ai-therepist!")
+from fastapi import FastAPI
+from pydantic import BaseModel
+import uvicorn
 
+app = FastAPI()
+
+
+class Query(BaseModel):
+    message: str
+
+
+@app.post("/ask")
+async def ask(query: Query):
+    # response = ai_agent(query)
+    response = "This is a response from the AI therapist."
+    return {"response": response}
 
 if __name__ == "__main__":
-    main()
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+
+
